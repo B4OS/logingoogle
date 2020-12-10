69f8882e3e499ef4f6355f40120e4df2969f418d
B4OS <brennoaraujo@yahoo.com>
2020-12-09 22:26:37 -0300

    integração com o cep dos correios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('ATIVIDADE 01') }}</div>
+                <div class="card-header">{{ __('RELOU MAI FRIENDE') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,112 @@
                         </div>
                     @endif
 
-                    {{ __('OLÁ MY FIREND') }}
+                    {{ __('Digite um CEP, por favor...') }}
+
+
+
+
+    <html>
+    <head>
+    <title>ViaCEP Webservice</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    
+    <script>
+    
+    function limpa_formulário_cep() {
+            
+            document.getElementById('rua').value=("");
+            document.getElementById('bairro').value=("");
+            document.getElementById('cidade').value=("");
+            document.getElementById('uf').value=("");
+            document.getElementById('ibge').value=("");
+    }
+
+    function meu_callback(conteudo) {
+        if (!("erro" in conteudo)) {
+            
+            document.getElementById('rua').value=(conteudo.logradouro);
+            document.getElementById('bairro').value=(conteudo.bairro);
+            document.getElementById('cidade').value=(conteudo.localidade);
+            document.getElementById('uf').value=(conteudo.uf);
+            document.getElementById('ibge').value=(conteudo.ibge);
+        } 
+        else {
+           
+            limpa_formulário_cep();
+            alert("CEP não encontrado.");
+        }
+    }
+        
+    function pesquisacep(valor) {
+
+       
+        var cep = valor.replace(/\D/g, '');
+
+        
+        if (cep != "") {
+
+           
+            var validacep = /^[0-9]{8}$/;
+
+            if(validacep.test(cep)) {
+
+               
+                document.getElementById('rua').value="...";
+                document.getElementById('bairro').value="...";
+                document.getElementById('cidade').value="...";
+                document.getElementById('uf').value="...";
+                document.getElementById('ibge').value="...";
+
+                
+                var script = document.createElement('script');
+
+                
+                script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+                
+                document.body.appendChild(script);
+
+            } 
+            else {
+                
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } 
+        else {
+            
+            limpa_formulário_cep();
+        }
+    };
+
+    </script>
+    </head>
+
+    <body>
+   
+
+   <form  method="get" action=".">
+   <br> 
+        <label>Cep:
+        <input name="cep" type="text" class="form-control" id="cep" value="" size="10" maxlength="9"
+               onblur="pesquisacep(this.value);" /></label><br />
+        <label>Rua:
+        <input name="rua" type="text" class="form-control" id="rua" size="60" /></label><br />
+        <label>Bairro:
+        <input name="bairro" type="text" class="form-control" id="bairro" size="40" /></label><br />
+        <label>Cidade:
+        <input name="cidade" type="text" class="form-control" id="cidade" size="40" /></label><br />
+        <label>Estado:
+        <input name="uf" type="text" class="form-control" id="uf" size="2" /></label><br />
+        <label>IBGE:
+        <input name="ibge" type="text" class="form-control" id="ibge" size="8" /></label><br />
+      </form>
+      
+    </body>
+
+    </html>
 
                 </div>
             </div>
